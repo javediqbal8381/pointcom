57f5177f47527d00832413162e44f368079da71b
javed iqbal <javediqbal8381@gmail.com>
2022-11-08 01:56:29 -0800

    setted frot-end

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">pointcom</a>
+      </header>
+      <main>listproducts</main>
+    </div>
+  );
+}
+
+export default App;
